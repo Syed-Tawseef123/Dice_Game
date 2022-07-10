3fdb6dac66764eab3ea6c90404ed0a9b284fe1c5
Syed Tajamul <87966592+Syed-Tawseef123@users.noreply.github.com>
2022-07-10 13:10:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var randomNumber=Math.floor(Math.random()*6+1);
 var randomImage="dice"+randomNumber+".png";
-var randomImageSrc="images/"+randomImage;
-document.querySelectorAll("img")[0].setAttribute("src",randomImageSrc);
+document.querySelectorAll("img")[0].setAttribute("src",randomImage);
 
 var random1=Math.floor(Math.random()*6+1);
 var randomImage1="dice"+random1+".png";
-var randomImageSrc1="images/"+randomImage1;
-document.querySelectorAll("img")[1].setAttribute("src",randomImageSrc1);
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImage1);
 
 if( randomNumber>random1){
   document.querySelector("h1").innerHTML="Player 1 wins";
